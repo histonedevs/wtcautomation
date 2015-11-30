8fc5b8f2613dfa87d86e37adfaac3c660a6f4837
Yasir Hussain <info@histone.com.pk>
2015-11-30 17:57:00 +0500

    correct migration

---
+++ database/migrations/2015_11_30_125112_add_branding_fields_to_accounts.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBrandingFieldsToAccounts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->string("company_name")->nullable();
+            $table->string("contact_person")->nullable();
+            $table->string("website")->nullable();
+            $table->string("log")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropColumn("company_name");
+            $table->dropColumn("contact_person");
+            $table->dropColumn("website");
+            $table->dropColumn("log");
+        });
+    }
+}
