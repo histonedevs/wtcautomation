63be373819597620c6b62f8962f7b581fdeb31b3
Yasir Hussain <info@histone.com.pk>
2015-12-04 13:17:59 +0500

    correct link

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -32,7 +32,7 @@ class AccountController extends Controller
             make_column('name' , 'accounts.name', 'Name', 'text'),
             make_column('email' , 'accounts.email', 'Email', 'text'),
             make_column('edit', null, '', null, [], '<a class="btn btn-primary" href="{{url("accounts/edit/".$id)}}">Edit</a>', null, '0px', null, false),
-            make_column('campaign', null, '', null, [], '<a class="btn btn-primary" href="{{url("campaigns/list/".$id)}}">Campaigns</a>', null, '0px', null, false),
+            make_column('campaign', null, '', null, [], '<a class="btn btn-primary" href="{{url("campaigns/index/".$id)}}">Campaigns</a>', null, '0px', null, false),
             make_column('delete', null, '', null, [], '<a class="btn btn-danger delete_account" href="#" path="{{url("accounts/delete/".$id)}}">Delete</a>', null, '0px', null, false),
         ];
 
