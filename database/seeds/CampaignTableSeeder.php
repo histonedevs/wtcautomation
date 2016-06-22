7dcdbdef1b81d356a22dd264602711f7fefe54b9
taskeen <taskeen.fatima@ymail.com>
2016-06-22 14:11:03 +0500

    Updated seeders for products and campaigns

--- database/seeds/CampaignTableSeeder.php
+++ database/seeds/CampaignTableSeeder.php
@@ -12,12 +12,14 @@ class CampaignTableSeeder extends Seeder
     public function run()
     {
         $now = date('Y-m-d H:i:s');
-        DB::table('campaigns')->insert([
-            'name' => str_random(10),
-            'user_id' => 1,
-            'product_id' => 1,
-            'created_at' => $now,
-            'updated_at' => $now
-        ]);
+        for($itr = 0; $itr < 10; $itr++) {
+            DB::table('campaigns')->insert([
+                'name' => str_random(10),
+                'user_id' => 1,
+                'product_id' => rand(1, 10),
+                'created_at' => $now,
+                'updated_at' => $now
+            ]);
+        }
     }
 }
--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -12,23 +12,25 @@ class ProductTableSeeder extends Seeder
     public function run()
     {
         $now = date('Y-m-d H:i:s');
-        DB::table('products')->insert([
-            'unique_id' => 1,
-            'product_id_type' => 1,
-            'user_id' => 1,
-            'title' => str_random(10),
-            'description' => str_random(20),
-            'sku' => str_random(10),
-            'product_id' => str_random(10),
-            'asin' => str_random(10),
-            'price' => 1200,
-            'shipping_price' => 1200,
-            'buybox_price' => 1200,
-            'sales_rank' => 4,
-            'stock' => 10,
-            'open_date' => $now,
-            'created_at' => $now,
-            'updated_at' => $now
-        ]);
+        for($itr = 0; $itr < 10; $itr++) {
+            DB::table('products')->insert([
+                'unique_id' => rand(),
+                'product_id_type' => 1,
+                'user_id' => 1,
+                'title' => str_random(10),
+                'description' => str_random(20),
+                'sku' => str_random(10),
+                'product_id' => str_random(10),
+                'asin' => str_random(10),
+                'price' => 1200,
+                'shipping_price' => 1200,
+                'buybox_price' => 1200,
+                'sales_rank' => 4,
+                'stock' => 10,
+                'open_date' => $now,
+                'created_at' => $now,
+                'updated_at' => $now
+            ]);
+        }
     }
 }
